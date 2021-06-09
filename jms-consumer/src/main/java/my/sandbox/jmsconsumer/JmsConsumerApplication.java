ef2279848b5d10c9d3616c116b70970ecb0a6796
ivanmikhalchanka <ivan.mikhalchanka@expcapital.com>
2021-06-09 19:46:25 +0300

    jms-consumer module generated

---
+++ jms-consumer/src/main/java/my/sandbox/jmsconsumer/JmsConsumerApplication.java
@@ -0,0 +1,13 @@
+package my.sandbox.jmsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsConsumerApplication.class, args);
+    }
+
+}
